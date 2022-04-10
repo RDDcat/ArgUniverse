31984e871c1911d59c69f0ff2e994592bc68cee3
dogeonK <kimdogeon1@gmail.com>
2022-04-11 02:49:07 +0900

    [UPDATE] 파일저장

--- android/app/src/main/java/kr/ac/kpu/arguniverse/MainActivity.kt
+++ android/app/src/main/java/kr/ac/kpu/arguniverse/MainActivity.kt
@@ -8,7 +8,9 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import kr.ac.kpu.arguniverse.MyModel.Companion.array1
 import kr.ac.kpu.arguniverse.databinding.ActivityMainBinding
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn : Button
@@ -26,21 +28,9 @@ class MainActivity : AppCompatActivity() {
         btn = findViewById(R.id.btn)
         heart = findViewById(R.id.heart)
         reed = findViewById(R.id.reed)
-        popular = MyModel("케이크 논쟁", 12, 3, 0,"\n" +
-                "친구가 너 생일이라 케이크를 삼\n" +
-                "\n" +
-                "근데 친구가 케이크 사놓고 너 기다리고 있는데\n" +
-                "배도 고프고 딱히 밥 먹을 곳도 없고 해서\n" +
-                "케이크 5분의 1조각 정도 칼로 썰어서 덜어먹음\n" +
-                "\n" +
-                "너가 딱 케이크 받았을 땐 케이크가 잘려있길래 물어보니까\n" +
-                "“내가 너 기다리다가 너무 배고파서 먹었어… 미안해….ㅜㅜㅜㅜ”\n" +
-                "라고 하면 너네 기분 어떰?\n" +
-                "\n" +
-                "전자는 아니 아무리 그래도 나 줄 거라면서 그걸 먹냐\n" +
-                "후자는 너무 배고팠으면 그럴 수 있지 받은 것만으로도 기분 좋다\n" +
-                "\n" +
-                "이거 의외로 갈림")
+        test()
+        popular = MyModel(array1[0].title,array1[0].fireCount, array1[0].viewCount, array1[0].postID,
+            array1[0].content)
 
 
         btn.setOnClickListener{
@@ -73,7 +63,7 @@ class MainActivity : AppCompatActivity() {
 
     fun initializelist() { //임의로 데이터 넣어서 만들어봄
         with(mDatas) {
-            add(MyModel("깬잎논쟁", 123, 112,1,"(설사 지인이라 할지라도) ‘외간 이성’이 붙어있는 깻잎지를 잘 못 떼어먹을 때, 자신의 애인(배우자)이 젓가락으로 깻잎지를 눌러줘도 되는지 ‘허용’과 ‘불허’의 갈림길에 선 건데요."))
+            add(MyModel("깻잎논쟁", 123, 112,1,"(설사 지인이라 할지라도) ‘외간 이성’이 붙어있는 깻잎지를 잘 못 떼어먹을 때, 자신의 애인(배우자)이 젓가락으로 깻잎지를 눌러줘도 되는지 ‘허용’과 ‘불허’의 갈림길에 선 건데요."))
             add(MyModel("모프는", 23, 21,2,"모프를 전공으로 하면 돈을 잘 벌까?"))
             add(MyModel("이상호", 3, 3,3,"이상호 교수님은 진짜 탈론을 잘할까?"))
             add(MyModel("교수님", 4, 412,4,"교수님들은 예쁜 여학생한테 더 잘챙겨 주실까?"))
--- android/app/src/main/java/kr/ac/kpu/arguniverse/MyModel.kt
+++ android/app/src/main/java/kr/ac/kpu/arguniverse/MyModel.kt
@@ -1,9 +1,50 @@
 package kr.ac.kpu.arguniverse
 
-data class MyModel(
-    val title :String, // 제목
-    val fireCount : Int, // 공감(활활타오르는거)
-    val viewCount : Int, // 조회수
-    val postID : Int, // 글 아이디
-    val content : String // 내용
-)
+import org.json.JSONObject
+import java.io.File
+import java.io.FileInputStream
+
+class MyModel(
+    val title: String,
+    val fireCount: Int,
+    val viewCount: Int,
+    val postID: Int,
+    val content: String
+) {
+
+
+    companion object {
+        var array1 = ArrayList<MyModel>()
+    }
+
+
+
+}
+
+
+fun test(): ArrayList<MyModel> {
+
+
+    val jsonString = openFileOutput("/data/data/kr.ac.kpu.arguniverse/files/MyModel.txt")
+
+    val jsonObject = JSONObject(jsonString)
+    val jsonArray = jsonObject.getJSONArray("person")
+    //val jsonObject = JSONObject(jsonString)
+    // val jsonArray = jsonObject.getJSONArray("person")
+
+    for (i in 0..jsonArray.length() - 1) {
+
+        val iObject = jsonArray.getJSONObject(i)
+        MyModel.array1.add(
+            MyModel(
+                iObject.getString("title"),
+                iObject.getInt("fireCount"),
+                iObject.getInt("viewCount"),
+                iObject.getInt("postID"),
+                iObject.getString("content")
+            )
+        )
+
+    }
+    return MyModel.array1
+}
