77503a4d16c5a513bdc54bad8aadfd8644dea0e6
dogeonK <kimdogeon1@gmail.com>
2022-04-29 17:40:21 +0900

    [UPDATE]  JSON MAPPING

---
+++ arguniverse/src/main/java/com/kyp/arguniverse/PostModel.java
@@ -0,0 +1,17 @@
+package com.kyp.arguniverse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Model {
+    private String title;
+    private int fireCount;
+    private int viewCount;
+    private int postID;
+    private String content;
+
+}
